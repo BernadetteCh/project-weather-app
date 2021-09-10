9a4c7a5301eadd6169aeef07ea53ee97fdb8dade
BernadetteCh <b.christiner93@gmail.com>
2021-09-10 20:26:28 +0200

    Added weather icon

--- java.js
+++ java.js
@@ -82,6 +82,12 @@ function displayWeatherCondition(response) {
   weatherdescription.innerHTML = response.data.weather[0].description;
   let date = document.querySelector("#date");
   date.innerHTML = formatDate(response.data.dt * 1000);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
