dd3d6f67d9da8ff609d44da624fc65c24c6a64b7
BernadetteCh <b.christiner93@gmail.com>
2021-12-29 21:39:15 +0100

    no relevant changes

--- java.js
+++ java.js
@@ -134,7 +134,7 @@ function getForecast(coordinates) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Frid", "Sat", "Sun"];
   return days[day];
 }
 
